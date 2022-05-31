3360fcd332c02e180dd93d08b0dd12dfeeced36d
Hector Galindo <hhgphicor@gmail.com>
2022-05-31 14:22:35 -0500

    Ughh esto esta llevando demasiado tiempo

--- retos/reto-13/main.c
+++ retos/reto-13/main.c
@@ -15,16 +15,49 @@
 
 int main(){
 
-    float promedio[5][6];
+    int promedio[4][5];
     char subjects[5][15];
 
-    // Introducimos las materias 
-
+    
     printf("Ingrese las materias que va a promediar: \n");
 
-    for(int sub = 0; sub <= 5; sub++){
-        printf("Materia [%i]: ", sub);
-        scanf("%c", &subjects[sub]);
+    
+    for(int sub = 0; sub < 5; sub++){
+        printf("Materia [%i]: ", sub+1);
+        scanf("%s", &subjects[sub]);
     }
 
-}
+       
+    for (int filas = 0; filas <= 4; filas++){
+        int suma = promedio[filas][5];
+
+        for (int columnas = 0; columnas <= 5; columnas++){
+            int nota;   
+            
+           if(columnas != 5){
+            printf("ingrese la nota [%i] de %s: ", columnas+1, subjects[filas]); 
+            scanf("%i", &nota);
+            printf("columna: %i, fila: %i\n",columnas, filas);
+            suma += promedio[filas][columnas];
+
+            if(nota <= 10 && nota >= 6){        
+               promedio[filas][columnas] = nota;
+               printf("La nota es: %i\n", promedio[filas][columnas]);
+               
+            }else{                             
+                while(nota < 6 || nota > 10){   
+                    printf("Ingrese una nota entre 6 y 10: ");
+                    scanf("%i", &nota);
+                    promedio[filas][columnas] = nota;
+                    printf("La nota es: %i\n", promedio[filas][columnas]);
+                }
+            }
+           }
+        }
+            printf("El promedio es: %i\n", suma);
+            printf("fila %i, columna 5: %i\n", filas, promedio[filas][5]);
+    }
+        
+    return 0; 
+    }
+
