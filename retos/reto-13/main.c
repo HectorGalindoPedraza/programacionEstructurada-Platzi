91cf1051573aa83ff41b9e728e1add2b3f1ec826
hecgph <hec.gph@proton.me>
2022-06-01 09:42:39 -0500

    Sigo intentando que esto funcione

--- retos/reto-13/main.c
+++ retos/reto-13/main.c
@@ -16,11 +16,11 @@
 int main(){
 
     int promedio[4][5];
+    int nota;
     char subjects[5][15];
 
     
     printf("Ingrese las materias que va a promediar: \n");
-
     
     for(int sub = 0; sub < 5; sub++){
         printf("Materia [%i]: ", sub+1);
@@ -29,33 +29,35 @@ int main(){
 
        
     for (int filas = 0; filas <= 4; filas++){
-        int suma = promedio[filas][5];
+        int suma;
 
         for (int columnas = 0; columnas <= 5; columnas++){
-            int nota;   
-            
+               
            if(columnas != 5){
             printf("ingrese la nota [%i] de %s: ", columnas+1, subjects[filas]); 
             scanf("%i", &nota);
-            printf("columna: %i, fila: %i\n",columnas, filas);
             suma += promedio[filas][columnas];
+            printf("columna: %i, fila: %i\n",columnas, filas);
 
-            if(nota <= 10 && nota >= 6){        
-               promedio[filas][columnas] = nota;
-               printf("La nota es: %i\n", promedio[filas][columnas]);
+                if(nota <= 10 && nota >= 6){        
+                promedio[filas][columnas] = nota;
+                printf("La nota es: %i\n", promedio[filas][columnas]);
                
-            }else{                             
-                while(nota < 6 || nota > 10){   
-                    printf("Ingrese una nota entre 6 y 10: ");
-                    scanf("%i", &nota);
-                    promedio[filas][columnas] = nota;
-                    printf("La nota es: %i\n", promedio[filas][columnas]);
+                }else{                             
+                    while(nota < 6 || nota > 10){   
+                        printf("Ingrese una nota entre 6 y 10: ");
+                        scanf("%i", &nota);
+                        promedio[filas][columnas] = nota;
+                        suma += promedio[filas][columnas];
+                        printf("La nota es: %i\n", promedio[filas][columnas]);
+                    }
                 }
-            }
+           }else{
+               suma /= 5;
+               promedio[filas][columnas] = suma;
+               printf("El promedio de la lista %i es: %i\n", filas, promedio[filas][columnas]);
            }
         }
-            printf("El promedio es: %i\n", suma);
-            printf("fila %i, columna 5: %i\n", filas, promedio[filas][5]);
     }
         
     return 0; 
